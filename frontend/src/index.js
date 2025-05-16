da973484ef9e5eaacd68627f5b9b430947c446e2
Kevin Sylvain <ksylvain77@gmail.com>
2025-05-16 12:46:33 -0400

    Update frontend configuration for proper production build

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -38,9 +38,7 @@ class ErrorBoundary extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>
 );
