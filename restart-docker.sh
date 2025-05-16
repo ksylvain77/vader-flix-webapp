79e9f2bcd0619df0adb5fbb7ff6c3a820fabbe18
Kevin Sylvain <ksylvain77@gmail.com>
2025-05-16 06:28:06 -0400

    chore: tweak restart script to add 10s wait and container status check

--- restart-docker.sh
+++ restart-docker.sh
@@ -11,3 +11,16 @@ echo "Starting vader-flix containers..."
 docker-compose up -d
 
 echo "Containers restarted at $(date)"
+
+echo "Waiting 10 seconds for containers to start..."
+sleep 10
+
+echo "Checking container status..."
+RUNNING_CONTAINERS=$(docker ps --filter "name=vader-flix" --format "{{.Names}}: {{.Status}}")
+if [ -n "$RUNNING_CONTAINERS" ]; then
+  echo "\nContainers are up and running:"
+  echo "$RUNNING_CONTAINERS"
+  echo "All vader-flix containers appear to be running."
+else
+  echo "\nWarning: No vader-flix containers are currently running!"
+fi
