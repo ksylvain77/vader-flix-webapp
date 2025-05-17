f749ece679c79e18d5dc302e3610ae28d91251a8
Kevin Sylvain <ksylvain77@gmail.com>
2025-05-17 16:38:26 -0400

    Fix auth middleware import in sonarr routes

--- backend/src/routes/sonarr.routes.js
+++ backend/src/routes/sonarr.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const sonarrController = require('../controllers/sonarr.controller');
-const { verifyToken } = require('../middleware/auth.middleware');
+const { verifyToken } = require('../middleware/auth');
 
 // All routes require authentication
 router.use(verifyToken);
