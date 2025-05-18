728ea0b8847b1908d2044905e51132441f98997e
Kevin Sylvain <ksylvain77@gmail.com>
2025-05-18 07:53:01 -0400

    fix: Remove unused ShowCard import

--- frontend/src/components/Sonarr.js
+++ frontend/src/components/Sonarr.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import SearchBar from './SearchBar';
-import ShowCard from './ShowCard';
 import LoadingSpinner from './LoadingSpinner';
 
 const Sonarr = () => {
